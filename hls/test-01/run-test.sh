9e23539bc0f4876ba098f38919038439cde08776
Viktar Halavachenka <v.halavachenka@yukonww.com>
2023-08-03 07:13:49 -0400

    added src

---
+++ hls/test-01/run-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /home/v.halavachenka/work/microchip-hls/vicg-hls-test-01/
+
+rm ./inputF-*.bmp
+rm ./outputF-*.bmp
+
+source /home/program/microchip/Libero_SoC_v2023.1/settings64.sh
+source /home/program/microchip/Libero_SoC_v2023.1/SmartHLS-2023.1/SmartHLS/examples/scripts/utils/autocomplete/bash_autocomplete.sh
+
+shls sw_run -a
