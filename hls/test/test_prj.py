0febc28a6268960ed5f6f2f6a24bb5152efa6f6f
Viktar Halavachenka <v.halavachenka@yukonww.com>
2023-08-04 05:04:46 -0400

    fix path

--- hls/test/test_prj.py
+++ hls/test/test_prj.py
@@ -6,12 +6,12 @@ import itertools
 
 def test_prj01():
     subprocess.check_call(
-        ['../prj-canny/run-shls.sh',
+        ['../prj-canny/build-shls.sh',
         "-d",
         "%s" % (os.path.abspath("../prj-canny"))])
 
 def test_prj02():
     subprocess.check_call(
-        ['../prj-sobel/run-shls.sh',
+        ['../prj-sobel/build-shls.sh',
         "-d",
         "%s" % (os.path.abspath("../prj-sobel"))])
