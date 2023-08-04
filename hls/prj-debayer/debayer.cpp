dda100e1484b65474b9fdfe956952d66b8e9fefd
Viktar Halavachenka <v.halavachenka@yukonww.com>
2023-08-04 10:12:24 -0400

    fix debayer. build-shls.sh work well, but build-cmake.sh don't work

--- hls/prj-debayer/debayer.cpp
+++ hls/prj-debayer/debayer.cpp
@@ -18,13 +18,9 @@ char output_File_name[100];
 char golden_File_name[100];
 
 using namespace hls;
-using cv::Mat;
-using vision::Img;
-using vision::PixelType;
-using vision::StorageType;
 
 // This line tests on a smaller image for faster co-simulation.
-#define FAST_COSIM
+// #define FAST_COSIM
 
 #ifdef FAST_COSIM
 #define WIDTH 100
@@ -47,12 +43,12 @@ using vision::StorageType;
 // lower the required Fmax in other resolutions as well, making hardware
 // implementation easier.
 using RgbImgT4PPC =
-    Img<PixelType::HLS_8UC3, HEIGHT, WIDTH, StorageType::FIFO, vision::NPPC_4>;
+    vision::Img<vision::PixelType::HLS_8UC3, HEIGHT, WIDTH, vision::StorageType::FIFO, vision::NPPC_4>;
 using BayerImgT4PPC =
-    Img<PixelType::HLS_8UC1, HEIGHT, WIDTH, StorageType::FIFO, vision::NPPC_4>;
+    vision::Img<vision::PixelType::HLS_8UC1, HEIGHT, WIDTH, vision::StorageType::FIFO, vision::NPPC_4>;
 
-template <PixelType PIXEL_T_IN, PixelType PIXEL_T_OUT, unsigned H, unsigned W,
-          StorageType STORAGE_IN, StorageType STORAGE_OUT,
+template <vision::PixelType PIXEL_T_IN, vision::PixelType PIXEL_T_OUT, unsigned H, unsigned W,
+          vision::StorageType STORAGE_IN, vision::StorageType STORAGE_OUT,
           vision::NumPixelsPerCycle NPPC = vision::NPPC_1>
 void DeBayerWrapper(vision::Img<PIXEL_T_IN, H, W, STORAGE_IN, NPPC> &ImgIn,
                     vision::Img<PIXEL_T_OUT, H, W, STORAGE_OUT, NPPC> &ImgOut,
@@ -62,8 +58,8 @@ void DeBayerWrapper(vision::Img<PIXEL_T_IN, H, W, STORAGE_IN, NPPC> &ImgIn,
     vision::DeBayer(ImgIn, ImgOut, BayerFormat);
 }
 
-template <PixelType PIXEL_T_IN, PixelType PIXEL_T_OUT, unsigned H, unsigned W,
-          StorageType STORAGE = vision::FIFO,
+template <vision::PixelType PIXEL_T_IN, vision::PixelType PIXEL_T_OUT, unsigned H, unsigned W,
+          vision::StorageType STORAGE = vision::FIFO,
           vision::NumPixelsPerCycle NPPC = vision::NPPC_1>
 void RGB2BayerWrapper(vision::Img<PIXEL_T_IN, H, W, STORAGE, NPPC> &ImgIn,
                       vision::Img<PIXEL_T_OUT, H, W, STORAGE, NPPC> &ImgOut)
@@ -109,36 +105,37 @@ int main(int argc, char *argv[])
 
     // Read input image into a cv Mat and convert it to RGB format since cv
     // reads images in BGR format
-    Mat BGRInMat = cv::imread(input_File_name, cv::IMREAD_COLOR);
-    Mat RGBInMat;
+    cv::Mat BGRInMat = cv::imread(input_File_name, cv::IMREAD_COLOR);
+    cv::Mat RGBInMat;
     cv::cvtColor(BGRInMat, RGBInMat, cv::COLOR_BGR2RGB);
 
     // Convert the cv Mat into Img class
     convertFromCvMat(RGBInMat, InImg);
-    // // Process the input, converting RGB to bayer and bayer back to RGB
-    // RGB2BayerWrapper(InImg, BayerImg);
-    // DeBayerWrapper(BayerImg, OutImg, 0);
+    // Process the input, converting RGB to bayer and bayer back to RGB
+    RGB2BayerWrapper(InImg, BayerImg);
+    DeBayerWrapper(BayerImg, OutImg, 0);
 
-    // // Convert the output image to cv Mat for comparing with the input image.
-    // // As we convert the RGB input image to bayer format and convert back to
-    // // RGB, the input and output images should be similar
-    // Mat OutMat;
-    // vision::convertToCvMat(OutImg, OutMat);
+    // Convert the output image to cv Mat for comparing with the input image.
+    // As we convert the RGB input image to bayer format and convert back to
+    // RGB, the input and output images should be similar
+    cv::Mat OutMat;
+    vision::convertToCvMat(OutImg, OutMat);
 
-    // // Compare the output image with the input image
-    // // Converting RGB to bayer format results in 3x reduction in data size, and
-    // // converting back from bayer to RGB (using debayer function) will result in
-    // // mismatch.
-    // // So we will say pass as long as less than 5% of pixels (each channel) have
-    // // mismatch greater than 32 (in range of 0-255).
-    // float ErrPercent = vision::compareMat(RGBInMat, OutMat, 32);
-    // printf("Percentage of over threshold: %0.2lf%\n", ErrPercent);
-    // if (ErrPercent < 5)
-    // {
-    //     // cv::imwrite(output_File_name, OutMat);
-    //     printf("PASS");
-    //     return 0;
-    // }
-    // printf("FAIL");
+    cv::imwrite(output_File_name, OutMat);
+
+    // Compare the output image with the input image
+    // Converting RGB to bayer format results in 3x reduction in data size, and
+    // converting back from bayer to RGB (using debayer function) will result in
+    // mismatch.
+    // So we will say pass as long as less than 5% of pixels (each channel) have
+    // mismatch greater than 32 (in range of 0-255).
+    float ErrPercent = vision::compareMat(RGBInMat, OutMat, 32);
+    printf("Percentage of over threshold: %0.2lf%\n", ErrPercent);
+    if (ErrPercent < 5)
+    {
+        printf("PASS");
+        return 0;
+    }
+    printf("FAIL");
     return 1;
 }
