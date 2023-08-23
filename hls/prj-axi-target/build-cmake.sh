9c30780743d1a16eb117b2a5c3f5eeb5d1933df2
Viktar Halavachenka <v.halavachenka@yukonww.com>
2023-08-23 03:46:34 -0400

    littel bit changes into build-cmake.sh

--- hls/prj-axi-target/build-cmake.sh
+++ hls/prj-axi-target/build-cmake.sh
@@ -2,6 +2,7 @@
 SCRIPT_NAME=`basename "$0"`
 
 BUID_DIR=./build
+USR_LIB_DIR=../../libraries
 
 echo "Start building"
 
@@ -9,19 +10,18 @@ if [[ ! -d $BUID_DIR ]]; then
     mkdir -p build
 fi
 
-cd build
+cd $BUID_DIR
 
-LIB_DIR=../../libraries
-if [[ ! -d $LIB_DIR ]]; then
-    mkdir $LIB_DIR || exit 1
+if [[ ! -d $USR_LIB_DIR ]]; then
+    mkdir $USR_LIB_DIR || exit 1
 fi
 
-if [[ ! -d $LIB_DIR/tclap-1.2.5 ]]; then
-    wget https://github.com/xguerin/tclap/archive/refs/tags/v1.2.5.tar.gz -O - | tar -xz -C $LIB_DIR || exit 1
+if [[ ! -d $USR_LIB_DIR/tclap-1.2.5 ]]; then
+    wget https://github.com/xguerin/tclap/archive/refs/tags/v1.2.5.tar.gz -O - | tar -xz -C $USR_LIB_DIR || exit 1
 fi
 
-if [[ ! -d $LIB_DIR/toml11-3.7.1 ]]; then
-    wget https://github.com/ToruNiina/toml11/archive/refs/tags/v3.7.1.tar.gz -O - | tar -xz -C $LIB_DIR || exit 1
+if [[ ! -d $USR_LIB_DIR/toml11-3.7.1 ]]; then
+    wget https://github.com/ToruNiina/toml11/archive/refs/tags/v3.7.1.tar.gz -O - | tar -xz -C $USR_LIB_DIR || exit 1
 fi
 
 cmake ..
