898cc00951b9149bbc3b84c142824ae1c0caea13
Viktar Halavachenka <v.halavachenka@yukonww.com>
2023-08-25 04:40:54 -0400

    user if-ctrl: Legacy AXI4 Slave Interface + without #pragma HLS function dataflow, but with #pragma HLS function pipeline - hls rtl_synth passed OK

--- hls/prj-canny-fifo-switch/axi-target.h
+++ hls/prj-canny-fifo-switch/axi-target.h
@@ -3,10 +3,10 @@
 
 #include <cstdint>
 
-#define USR_CTRL_HLS_FUNCTION_DATAFLOW
+// #define USR_CTRL_HLS_FUNCTION_DATAFLOW
 #define USR_CTRL_HLS_FUNCTION_PIPELINE
 
-// struct glob_st {
+// struct gaussian_filter_st {
 //     uint32_t status;
 //     uint32_t ctrl;
 // };
@@ -14,11 +14,7 @@
 struct AxiTargetReg_st {
     uint32_t ctrl;
     uint32_t status;
-    // glob_st glob;
-    // uint8_t gaussian_filter;
-    // uint8_t sobel_filter;
-    // uint8_t nonmaximum_suppression;
-    // uint8_t hysteresis_filter;
+    // gaussian_filter_st gf;
 };
 
 #endif  // __AXI_TARGET_H__
--- hls/prj-canny-fifo-switch/rtl-canny.cpp
+++ hls/prj-canny-fifo-switch/rtl-canny.cpp
@@ -1,7 +1,7 @@
 #include "axi-target.h"
 #include "define.hpp"
 
-// #pragma HLS interface variable(axi_reg) type(axi_slave) concurrent_access(true)
+#pragma HLS interface variable(axi_reg) type(axi_slave) concurrent_access(true)
 struct AxiTargetReg_st axi_reg;
 
 void canny(hls::ap_uint< 1 > switch_0, hls::ap_uint< 1 > switch_1, hls::ap_uint< 1 > switch_2,
@@ -17,13 +17,13 @@ void canny(hls::ap_uint< 1 > switch_0, hls::ap_uint< 1 > switch_1, hls::ap_uint<
     hls::FIFO< unsigned char > output_fifo_nm(/* depth = */ 2);
 
     // axi_reg.status++;
-    // hls::ap_uint< 1 > switch_0 = 1;  // axi_reg.ctrl & 0x01;
-    // hls::ap_uint< 1 > switch_1 = 1;  // axi_reg.ctrl & 0x02;
-    // hls::ap_uint< 1 > switch_2 = 1;  // axi_reg.ctrl & 0x04;
-    // hls::ap_uint< 1 > switch_3 = 1;  // axi_reg.ctrl & 0x08;
+    bool switch_i0 = (axi_reg.ctrl & 0x01) ? true : false;  //
+    bool switch_i1 = (axi_reg.ctrl & 0x02) ? true : false;  //
+    bool switch_i2 = (axi_reg.ctrl & 0x04) ? true : false;  //
+    bool switch_i3 = (axi_reg.ctrl & 0x08) ? true : false;  //
 
-    gaussian_filter(switch_0, /*axi_reg.ctrl & 0x01,*/ input_fifo, output_fifo_gf);
-    sobel_filter(switch_1, /*axi_reg.ctrl & 0x02,*/ output_fifo_gf, output_fifo_sf);
-    nonmaximum_suppression(switch_2, /*axi_reg.ctrl & 0x04,*/ output_fifo_sf, output_fifo_nm);
-    hysteresis_filter(switch_3, /*axi_reg.ctrl & 0x08,*/ output_fifo_nm, output_fifo);
+    gaussian_filter(switch_i0, /*axi_reg.ctrl & 0x01,*/ input_fifo, output_fifo_gf);
+    sobel_filter(switch_i1, /*axi_reg.ctrl & 0x02,*/ output_fifo_gf, output_fifo_sf);
+    nonmaximum_suppression(switch_i2, /*axi_reg.ctrl & 0x04,*/ output_fifo_sf, output_fifo_nm);
+    hysteresis_filter(switch_i3, /*axi_reg.ctrl & 0x08,*/ output_fifo_nm, output_fifo);
 }
